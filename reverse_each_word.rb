b9b2175a12dcd177bc6b1c9b59d1357b9c8e9a07
Jonathan Chan <johnkchan252@gmail.com>
2018-08-15 03:02:06 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -8,8 +8,8 @@ def reverse_each_word(sentence)
 end
 
 def reverse_each_word(sentence)
-  output = []
-  (sentence.split).each do |word|
+  output = sentence.split
+  output.collect do |word|
     output << word.reverse
   end
   output = output.join(" ")
